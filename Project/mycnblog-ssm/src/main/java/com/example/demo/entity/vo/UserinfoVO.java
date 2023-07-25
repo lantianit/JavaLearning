2cec1bbda9be86df9e3946b955cd01d6423d8f4f
学习追求高效率 <1210931886@qq.com>
2023-07-25 21:59:26 +0800

    在线OJ

--- Project/mycnblog-ssm/src/main/java/com/example/demo/entity/vo/UserinfoVO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity.vo;
-
-import com.example.demo.entity.Userinfo;
-import lombok.Data;
-
-@Data
-public class UserinfoVO extends Userinfo {
-    private Integer artCount; // 此人发表的文章总数
-}
