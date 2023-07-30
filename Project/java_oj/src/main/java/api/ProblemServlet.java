7dd73fa8c2fb043d8163024c54e2bfb653420c87
学习追求高效率 <1210931886@qq.com>
2023-07-30 23:05:39 +0800

    7.30 复盘

--- Project/java_oj/src/main/java/api/ProblemServlet.java
+++ Project/java_oj/src/main/java/api/ProblemServlet.java
@@ -10,11 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/problem")
 public class ProblemServlet extends HttpServlet {
-    private ObjectMapper objectMapper = new ObjectMapper();
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,20 +24,15 @@ public class ProblemServlet extends HttpServlet {
         resp.setContentType("application/json;charset=utf8");
 
         ProblemDAO problemDAO = new ProblemDAO();
-        // 尝试获取 id 参数. 如果能获取到, 说明是获取题目详情; 如果不能获取到, 说明是获取题目列表.
         String idString = req.getParameter("id");
-        if (idString == null || "".equals(idString)) {
-            // 没有获取到 id 字段. 查询题目列表
-            List<Problem> problems = problemDAO.selectAll();
-            String respString = objectMapper.writeValueAsString(problems);
+        if(idString == null || idString.equals("")){
+            List<Problem> list = problemDAO.selectAll();
+            String respString = objectMapper.writeValueAsString(list);
             resp.getWriter().write(respString);
         } else {
-            // 获取到了题目 id. 查询题目详情
             Problem problem = problemDAO.selectOne(Integer.parseInt(idString));
             String respString = objectMapper.writeValueAsString(problem);
             resp.getWriter().write(respString);
-
         }
-
     }
 }
