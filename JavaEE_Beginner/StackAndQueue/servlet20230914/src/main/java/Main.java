174e8c60603df2dd56b2e2e9b3399963fc801023
zhang-haoluck <1210931886@qq.com>
2023-09-14 21:49:41 +0800

    9.14 servlet复习

--- JavaEE_Beginner/StackAndQueue/servlet20230914/src/main/java/Main.java
+++ JavaEE_Beginner/StackAndQueue/servlet20230914/src/main/java/Main.java
@@ -1,61 +1,50 @@
-import java.io.BufferedReader;
-import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
-
-public class Main {
-    public static int n,m;
-    public static int[][] gap = new int[110][110];
-    public static int[][] dist = new int[110][110];
-    public static PII[] q = new PII[110*110];
-    public static int hh,tt;
-    public static Queue<Integer> queue = new ArrayDeque<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] s = rd.readLine().split(" ");
-        n = Integer.parseInt(s[0]);
-        m = Integer.parseInt(s[1]);
-        for (int i = 0; i < n; i++) {
-            String[] st = rd.readLine().split(" ");
-            for (int j = 0 ; j < m ;j ++ ) {
-                gap[i][j] = Integer.parseInt(st[j]);
-                dist[i][j] = -1;
-            }
-        }
-        System.out.println(bfs());
-        wt.close();
+import java.util.Scanner;
+public class Main{
+    static int N = 100010,n,m,idx,hh,tt;
+    static int[] h = new int[N],e = new int[N],ne = new int[N];
+    static int[] d = new int[N],q = new int[N];
+    //这个是单链表的模板
+    public static void add(int a,int b){
+        e[idx] = b;
+        ne[idx] = h[a];
+        h[a] = idx++;
     }
-
-    public static int  bfs(){
-        hh = 0 ; tt = -1; //队列的头节点=0，尾节点 = 0；
-        dist[0][0] = 0; // 我们首先站在的是第一个点，所以值距离设置为0
-        q[++tt] = new PII(0,0); //然后将第一个点下标存入q队列中
-        //利用向量的方法进行让他上下左右判断是否能够前进
-        int[] dx = {-1,0,1,0};//上(-1,0) 下(1,0)
-        int[] dy = {0,1,0,-1};//左(0,-1) 右(0,1)
+    public static int bfs(){
+        hh = 0 ; tt = -1;
+        d[1] = 0; // 第一个点是距离为0
+        q[++tt] = 1; //然后将第一个点加进去
+        //如果队列不是空的
         while(hh <= tt){
-            PII t = q[hh++]; //每一次将头结点拿出来
-            for(int i = 0 ; i < 4 ; i ++ ){//然后进行下一步要往哪里走，这里可能有多重选择可走
-                int x = t.first + dx[i]; //这里进行x轴向量判断
-                int y = t.second + dy[i];//这里进行y轴向量的判断
-                //如果x，y满足在地图中不会越界，然后地图上的点g是0(表示可以走)，
-                //然后这里是没走过的距离d是-1；
-                if(x >= 0 && x < n && y >= 0 && y < m && gap[x][y] == 0 && dist[x][y] == -1){
-                    //将现在可以走的点(x,y)加上上一个点计数距离的点加上一，就是现在走到的点的距离
-                    dist[x][y] = dist[t.first][t.second] + 1;
-                    q[++tt] = new PII(x,y);//然后将这一个可以走的点存入队列尾
+            int t = q[hh++]; //取出队头
+
+            //然后遍历一下单链表
+            for(int i = h[t] ; i != -1 ; i = ne[i]){
+
+                int s = e[i]; //然后这个点用一个变量表示
+                if(d[s] == -1){ //如果这个点是没走过的
+                    d[s] = d[t] + 1; //然后将这个点距离加上1
+                    q[++tt] = s;//然后将第二步的点加入到队尾中
                 }
             }
         }
-        return dist[n-1][m-1]; //最后返回的是地图走到尽头最后一个位置的位置统计的距离
+        return d[n]; //最后返回1到n的最短距离d
+
+
     }
-    static class  PII {
-        int first;
-        int second;
-        public PII(int first,int second) {
-            this.first = first;
-            this.second = second;
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        m = scan.nextInt();
+        //这里需要将距离跟头结点都赋值成-1
+        for(int i = 1 ; i < N ; i++){
+            h[i] = -1;
+            d[i] = -1;
         }
+        for(int i = 0 ; i < m ; i ++ ){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            add(a,b);
+        }
+        System.out.println(bfs());
     }
 }
---
+++ JavaEE_Beginner/StackAndQueue/servlet20230914/src/main/java/PostParameter.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/postParameter")
+public class PostParameter extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        String userId = req.getParameter("userId");
+        String classId = req.getParameter("classId");
+        resp.getWriter().write("userId: " + userId + ", " + "classId: " +
+classId);
+   }
+}
