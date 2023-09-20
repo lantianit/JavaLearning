74ee5cbd40f42f11450c0a64a07a98e548463d4a
zhang-haoluck <1210931886@qq.com>
2023-09-20 18:09:27 +0800

    9.20

--- JavaEEAdvanced/SpringBoot/src/main/java/com/example/demo/UserController.java
+++ JavaEEAdvanced/SpringBoot/src/main/java/com/example/demo/UserController.java
@@ -4,15 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @Author zh
- * @Date 2023/7/24 16:55
- * @PackageName:com.example.demo
- * @ClassName: UserController
- * @Description: TODO
- * @Version 1.0
- */
-
 @Controller
 @ResponseBody
 @RequestMapping("/user")
