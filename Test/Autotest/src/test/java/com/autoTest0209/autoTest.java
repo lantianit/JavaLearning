e9cbfb4e2dc37d84f2a7e1c6efbe110f93127a7b
zhang-haoluck <1210931886@qq.com>
2023-09-24 22:43:58 +0800

    9.24 测试

--- Test/Autotest/src/test/java/com/autoTest0209/autoTest.java
+++ Test/Autotest/src/test/java/com/autoTest0209/autoTest.java
@@ -16,6 +16,7 @@ import java.util.Set;
 public class autoTest {
     ChromeDriver driver = new ChromeDriver();
 
+
     public void baseConTrol() throws InterruptedException {
         driver.get("https://www.baidu.com");
         ///////
