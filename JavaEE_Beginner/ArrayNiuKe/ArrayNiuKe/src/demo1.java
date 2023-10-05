adb80bdb593cb701f3730bf1f9a089bce100d22f
zhang-haoluck <1210931886@qq.com>
2023-10-05 10:48:09 +0800

    10.5 数组

---
+++ JavaEE_Beginner/ArrayNiuKe/ArrayNiuKe/src/demo1.java
@@ -0,0 +1,10 @@
+public class demo1 {
+    public static void main(String[] args) {
+        String a = "123";
+        System.out.println(a.length());
+        char[] b = "123".toCharArray();
+        System.out.println(b.length);
+        char[] c = {'1','2','3'};
+        System.out.println(c.length);
+    }
+}
