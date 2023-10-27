6d6530d834991d64df16e1c0daad0f3b3ca4744d
zhang-haoluck <1210931886@qq.com>
2023-10-27 20:09:14 +0800

    10.27 学习

--- Project/blog-springboot-mp/src/test/java/t.java
+++ Project/blog-springboot-mp/src/test/java/t.java
@@ -1,31 +1,39 @@
-class MyBlockingQueue {
-    private int[] items;
-    private volatile int head = 0;
-    private volatile int tail = 0;
-    private volatile int size = 0;
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
 
-    public void put(int elem) throws InterruptedException {
-        synchronized (this) {
-            while (size >= items.length) {
-                this.wait();
-            }
-            items[tail] = elem;
-            tail++;
-            if (tail >= items.length) {
-                tail = 0;
-            }
-            size++;
-            this.notify();
-        }
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
     }
 
-    public Integer take() throws InterruptedException {
-        synchronized (this) {
-            while (size == 0) {
-                this.wait();
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue = new ArrayDeque<Node>();
+        queue.put(root);
+        while (queue.size() > 0) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                    Node n = queue.element();
+
             }
         }
-
     }
-
 }
