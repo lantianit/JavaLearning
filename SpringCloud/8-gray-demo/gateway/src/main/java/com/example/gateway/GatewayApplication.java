6e083663807f367f53e896e37e35c84ca04e6e8a
zhang-haoluck <1210931886@qq.com>
2024-01-08 21:35:30 +0800

    1.8

--- SpringCloud/8-gray-demo/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ SpringCloud/8-gray-demo/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 @LoadBalancerClients(defaultConfiguration =
         GrayLoadBalancerConfig.class)
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
