c370055656929fa2c194d084b5d2537410f1d874
zhang-haoluck <1210931886@qq.com>
2024-03-24 21:20:47 +0800

    3.24

---
+++ Project/reggie_take_out/reggie_take_out/src/main/java/com/itheima/reggie/Study.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie;
+
+import io.swagger.models.auth.In;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    private Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        String s = "1";
+        Map<Character, Integer> map = new HashMap<>();
+        map.
+    }
+}
