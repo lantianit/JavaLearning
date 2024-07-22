2055577a26b004c10d57508b547fad793d95fb5a
zhang-haoluck <1210931886@qq.com>
2024-07-22 23:48:02 +0800

    7.22

--- Project/mq_t/src/main/java/com/example/mq/study/MSGQueue2.java
+++ Project/mq_t/src/main/java/com/example/mq/study/MSGQueue2.java
@@ -1,40 +1,113 @@
-import java.util.HashMap;
-import java.util.Map;
+import com.example.mq.common.BinaryTool;
+import com.example.mq.common.MqException;
+import com.example.mq.mqserver.core.MSGQueue;
+import com.example.mq.mqserver.core.Message;
+
+import java.io.*;
 import java.util.Scanner;
-import java.util.Stack;
 
-class Main {
-
-    static int N = (int)1e5+10;
-    static int[] myStack = new int[N];
-    static int idx = 0;
-
-    public static void main(String[] args) {
-
-        Map<Character, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-
-        while (n-- != 0) {
-            String s = scan.next();
-            if (s.equals("push")) {
-                int x = scan.nextInt();
-                myStack[idx++] = x;
-            } else if (s.equals("pop")) {
-                idx--;
-                System.out.println(myStack[idx]);
-            } else if (s.equals("empty")) {
-                if (idx == 0) {
-                    System.out.println("Yes");
-                } else {
-                    System.out.println("No");
-                }
-            } else if (s.equals("query")) {
-                System.out.println(myStack[idx-1]);
+class StudyMessageFileManager {
+    
+    static public class Stat {
+        public int totalCount;
+        public int validCount;
+    }
+    
+    public void init() {}
+    
+    private String getQueueDir(String queueName) {
+        return "./data/" + queueName;
+    }
+    
+    private String getQueueDataPath(String queueName) {
+        return getQueueDir(queueName) + "/queue_data.txt";
+    }
+    
+    private String getQueueStatPath(String queueName) {
+        return getQueueDir(queueName) + "/queue_stat.txt";
+    }
+    
+    private Stat readStat(String queueName) throws IOException {
+        Stat stat = new Stat();
+        try (InputStream inputStream = new FileInputStream(getQueueDataPath(queueName))){
+            Scanner scanner = new Scanner(inputStream);
+            stat.totalCount = scanner.nextInt();
+            stat.validCount = scanner.nextInt();
+            return stat;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    private void writeStat(String queueName, Stat stat) {
+        try (OutputStream outputStream = new FileOutputStream(getQueueDataPath(queueName))) {
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.write(stat.totalCount + "\t" + stat.validCount);
+            printWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void createQueueFiles(String queueName) throws IOException {
+        File baseDir = new File(getQueueDir(queueName));
+        if (!baseDir.exists()) {
+            boolean ok = baseDir.mkdirs();
+            if (!ok) {
+                throw new IOException("创建目录失败！baseDir=" + baseDir.getAbsolutePath());
             }
         }
-
     }
 
+    public boolean checkFilesExits(String queueName) {
+        // 判定队列的数据文件和统计文件是否都存在!!
+        File queueDataFile = new File(getQueueDataPath(queueName));
+        if (!queueDataFile.exists()) {
+            return false;
+        }
+        File queueStatFile = new File(getQueueStatPath(queueName));
+        if (!queueStatFile.exists()) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void sendMessage(MSGQueue queue, Message message) throws MqException, IOException {
+        if (!checkFilesExits(queue.getName())) {
+            throw new MqException("[MessageFileManager] 队列对应的文件不存在! queueName=" + queue.getName());
+        }
+        byte[] messageBinary = BinaryTool.toBytes(message);
+        synchronized (queue) {
+            File queueDataFile = new File(getQueueDataPath(queue.getName()));
+            message.setOffsetBeg(queueDataFile.length() + 4);
+            message.setOffsetEnd(queueDataFile.length() + 4 + messageBinary.length);
+            try (OutputStream outputStream = new FileOutputStream(queueDataFile, true)) {
+                try (DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
+                    // 接下来要先写当前消息的长度, 占据 4 个字节的~~
+                    dataOutputStream.writeInt(messageBinary.length);
+                    // 写入消息本体
+                    dataOutputStream.write(messageBinary);
+                }
+            }
+            
+        }
+    }
+    
+    public void gc(MSGQueue queue) throws MqException, IOException {
+        synchronized (queue) {
+            long gcBeg = System.currentTimeMillis();
+            File queueDataNewFile = new File(getQueueDataPath(queue.getName()));
+            if (queueDataNewFile.exists()) {
+                // 正常情况下, 这个文件不应该存在. 如果存在, 就是意外~~ 说明上次 gc 了一半, 程序意外崩溃了.
+                throw new MqException("[MessageFileManager] gc 时发现该队列的 queue_data_new 已经存在! queueName=" + queue.getName());
+            }
+            boolean ok = queueDataNewFile.createNewFile();
+            if (!ok) {
+                throw new MqException()
+            }
+            
+        }
+    }
+    
 }
--- Project/mq_t/src/test/java/com/example/t.java
+++ Project/mq_t/src/test/java/com/example/t.java
@@ -1,61 +1,71 @@
 package com.example;
 
-import com.example.mq.MqApplication;
-import com.example.mq.mqserver.core.Binding;
-import com.example.mq.mqserver.core.Exchange;
-import com.example.mq.mqserver.core.ExchangeType;
 import com.example.mq.mqserver.core.MSGQueue;
-import com.example.mq.mqserver.datacenter.DataBaseManager;
-import org.apache.ibatis.javassist.bytecode.ExceptionTable;
+import com.example.mq.mqserver.datacenter.MessageFileManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.List;
-import java.util.TooManyListenersException;
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootTest
- class t {
+public class t {
     
-    private DataBaseManager dataBaseManager = new DataBaseManager();
+    private MessageFileManager messageFileManager = new MessageFileManager();
     
+    private static final String queueName1 = "testQueue1";
+    private static final String queueName2 = "testQueue2";
+
     @BeforeEach
-    public void setUp() {
-        MqApplication.context = SpringApplication.run(MqApplication.class);
-        dataBaseManager.init();
+    public void setUp() throws IOException {
+        // 准备阶段, 创建出两个队列, 以备后用
+        messageFileManager.createQueueFiles(queueName1);
+        messageFileManager.createQueueFiles(queueName2);
     }
-    
+
+    // 这个方法就是每个用例执行完毕之后的收尾工作
     @AfterEach
-    public void tearDown() {
-        MqApplication.context.close();
-        dataBaseManager.deleteDB();
+    public void tearDown() throws IOException {
+        // 收尾阶段, 就把刚才的队列给干掉.
+        messageFileManager.destroyQueueFiles(queueName1);
+        messageFileManager.destroyQueueFiles(queueName2);
+    }
+
+    @Test
+    public void testCreateFiles() {
+        // 创建队列文件已经在上面 setUp 阶段执行过了. 此处主要是验证看看文件是否存在.
+        File queueDataFile1 = new File("./data/" + queueName1 + "/queue_data.txt");
+        Assertions.assertEquals(true, queueDataFile1.isFile());
+        File queueStatFile1 = new File("./data/" + queueName1 + "/queue_stat.txt");
+        Assertions.assertEquals(true, queueStatFile1.isFile());
+
+        File queueDataFile2 = new File("./data/" + queueName2 + "/queue_data.txt");
+        Assertions.assertEquals(true, queueDataFile2.isFile());
+        File queueStatFile2 = new File("./data/" + queueName2 + "/queue_stat.txt");
+        Assertions.assertEquals(true, queueStatFile2.isFile());
     }
     
     @Test
-    public void testInitTable() {
-        List<Exchange> exchangeList = dataBaseManager.selectAllExchanges();
-        List<MSGQueue> queueList = dataBaseManager.selectAllQueues();
-        List<Binding> bindingList = dataBaseManager.selectAllBindings();
-
-        Assertions.assertEquals(1, exchangeList.size());
-        Assertions.assertEquals("", exchangeList.get(0).getName());
-        Assertions.assertEquals(ExchangeType.DIRECT, exchangeList.get(0).getType());
-        Assertions.assertEquals(0, queueList.size());
-        Assertions.assertEquals(0, bindingList.size());
+    public void testReadWriteStat() {
+        MessageFileManager.Stat stat = new MessageFileManager.Stat();
+        stat.validCount = 100;
+        stat.validCount = 50;
+        ReflectionTestUtils.invokeMethod(messageFileManager, "writeStat", queueName1, stat);
+        
+        MessageFileManager.Stat newStat = ReflectionTestUtils.invokeMethod(messageFileManager,"readStat", queueName1);
+        Assertions.assertEquals(100, newStat.totalCount);
+        Assertions.assertEquals(50, newStat.validCount);
+        System.out.println("");
     }
     
-    @Test
-    public void testInsertExchange() {
-        Exchange exchange = createTestExchange("testExchange");
+    private MSGQueue createTestQueue(String queueName) {
+        MSGQueue queue = new MSGQueue();
+        queue.setAutoDelete();
     }
 
-    private Exchange createTestExchange(String exchangeName) {
-        Exchange exchange = new Exchange();
-        exchange.setName(exchangeName);
-        exchange.setType(ExchangeType.DIRECT);
-    }
-
+    
 }
